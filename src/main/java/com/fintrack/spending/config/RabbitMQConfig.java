d744e2448ab5f136d1b3764f6613784f021fc6ba
Yamkela Venfolo <yamkela.venfolo@standardbank.co.za>
2026-05-20 22:48:41 +0200

    remove health probe on the security config

--- src/main/java/com/fintrack/spending/config/RabbitMQConfig.java
+++ src/main/java/com/fintrack/spending/config/RabbitMQConfig.java
@@ -1,8 +1,16 @@
 package com.fintrack.spending.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +18,27 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     @Bean
+    public TopicExchange transactionExchange(@Value("${fintrack.rabbitmq.exchange}") String exchange) {
+        return new TopicExchange(exchange, true, false);
+    }
+
+    @Bean
+    public Queue transactionQueue(@Value("${rabbitmq.queues.transactions}") String queue) {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Binding transactionBinding(Queue transactionQueue, TopicExchange transactionExchange,
+                                      @Value("${fintrack.rabbitmq.routing-key}") String routingKey) {
+        return BindingBuilder.bind(transactionQueue).to(transactionExchange).with(routingKey);
+    }
+
+    @Bean
     public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new Jackson2JsonMessageConverter(mapper);
     }
 
     @Bean
